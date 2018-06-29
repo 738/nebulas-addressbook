dab2bc17d2ca932474c87b686df6ff5d5ba8db7c
junwooji <junwoo@finda.co.kr>
2018-06-29 22:15:19 +0900

    add contractdatacontroller.js, config.js

---
+++ webapp/src/config.js
@@ -0,0 +1,10 @@
+const dappAddress = "n1jSHYLKmwdaZK5KczjUF7z2VZfFCMZDyiV";
+// mainnet: 
+// testnet: n1jSHYLKmwdaZK5KczjUF7z2VZfFCMZDyiV
+
+const networkAddress = "https://testnet.nebulas.io";
+// const networkAddress = "https://mainnet.nebulas.io";
+
+const ownerAddress = "n1Ry1HRT39EtXtk6XHSzUUhNe5k8Q9zmixm";
+
+export { dappAddress, networkAddress, ownerAddress };
